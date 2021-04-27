4b8846a6138d78dddcdf4774af7dc3bdecabd7ce
nicolaipiet <51484216+nicolaipiet@users.noreply.github.com>
2021-04-27 10:53:20 +0200

    init commit

---
+++ app.py
@@ -0,0 +1,194 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import shapefile
+import urllib.request
+import zipfile
+import geopandas as gpd
+import os
+import json
+
+
+from shapely.geometry import Polygon
+
+from bokeh.io import output_notebook, show, output_file, curdoc
+from bokeh.plotting import figure
+from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, NumeralTickFormatter, Slider, HoverTool, Select, Column
+from bokeh.palettes import brewer
+from bokeh.layouts import widgetbox, row, column
+from bokeh.application import Application
+from bokeh.application.handlers import FunctionHandler
+
+# Load data
+
+geo = gpd.read_file('preprocessed_data/geo.geojson')
+
+#---------------------------------------------------------------------------------
+# Format table for legend & interactivity
+
+#  This dictionary contains the formatting for the data in the plots
+format_data = [('tip_p', np.min(geo.tip_p), np.max(geo.tip_p),'0%', 'Tip percentage'),
+               ('tip_amount', 0, 25,'$0,0', 'Average Tip amount'),
+               ('fare_amount', 0, 50, '$0,0', 'Average fare amount'),
+               ('pickup', 0, np.max(geo.pickup),'0,0', 'Amount of pickups'),
+               ('dropoff', 0, np.max(geo.dropoff),'0,0', 'Amount of dropoffs'),
+              ]
+      
+ 
+#Create a DataFrame object from the dictionary 
+format_df = pd.DataFrame(format_data, columns = ['field' , 'min_range', 'max_range' , 'format', 'verbage'])
+
+#---------------------------------------------------------------------------------
+ 
+
+
+
+#---------------------------------------------------------------------------------
+# Plotting functions
+
+def json_data(selectedMonth):
+    M = selectedMonth
+    
+    # Pull selected year from neighborhood summary data
+    df_M = geo[geo['month'] == M]
+    
+     # Merge the GeoDataframe object (sf) with the neighborhood summary data (neighborhood)
+    merged =df_M
+    # Fill the null values
+    values = {'Month': M, 'tip_p': 0, 'tip_amount': 0, 'fare_amount': 0, 'pickup':0, 'dropoff':0}
+    merged = merged.fillna(value=values)
+    
+    # Bokeh uses geojson formatting, representing geographical features, with json
+    # Convert to json
+    merged_json = json.loads(merged.to_json())
+    
+    # Convert to json preferred string-like object 
+    json_data = json.dumps(merged_json)
+    return json_data
+
+# Define the callback function: update_plot
+def update_plot(attr, old, new):
+    # The input yr is the year selected from the slider
+    M = slider.value
+    new_data = json_data(M)
+    
+    # The input cr is the criteria selected from the select box
+    cr = select.value
+    input_field = format_df.loc[format_df['verbage'] == cr, 'field'].iloc[0]
+    
+    # Update the plot based on the changed inputs
+    p = make_plot(input_field)
+    
+    # Update the layout, clear the old document and display the new document
+    layout = column(p, widgetbox(select), widgetbox(slider))
+    curdoc().clear()
+    curdoc().add_root(layout)
+    
+    # Update the data
+    geosource.geojson = new_data 
+    
+# Create a plotting function
+def make_plot(field_name):    
+  # Set the format of the colorbar
+  min_range = format_df.loc[format_df['field'] == field_name, 'min_range'].iloc[0]
+  max_range = format_df.loc[format_df['field'] == field_name, 'max_range'].iloc[0]
+  field_format = format_df.loc[format_df['field'] == field_name, 'format'].iloc[0]
+
+  # Instantiate LinearColorMapper that linearly maps numbers in a range, into a sequence of colors.
+  color_mapper = LinearColorMapper(palette = palette, low = min_range, high = max_range)
+
+  # Create color bar.
+  format_tick = NumeralTickFormatter(format=field_format)
+  color_bar = ColorBar(color_mapper=color_mapper, label_standoff=18, formatter=format_tick,
+  border_line_color=None, location = (0, 0))
+
+  # Create figure object.
+  verbage = format_df.loc[format_df['field'] == field_name, 'verbage'].iloc[0]
+
+  p = figure(title = verbage + '- NYC in 2020', 
+             plot_height = 1000, plot_width = 1000,
+             toolbar_location = None)
+  p.xgrid.grid_line_color = None
+  p.ygrid.grid_line_color = None
+  p.axis.visible = False
+
+  # Add patch renderer to figure. 
+  p.patches('xs','ys', source = geosource, fill_color = {'field' : field_name, 'transform' : color_mapper},
+          line_color = 'black', line_width = 0.25, fill_alpha = 1)
+  
+  # Specify color bar layout.
+  p.add_layout(color_bar, 'right')
+
+  # Add the hover tool to the graph
+  p.add_tools(hover)
+  return p
+
+#-----------------------------------------------------------------------------------
+# Creating interactive map plot
+
+# Input geojson source that contains features for plotting for:
+# initial year 2018 and initial criteria sale_price_median
+geosource = GeoJSONDataSource(geojson = json_data(1))
+input_field = 'tip_p'
+
+# Define a sequential multi-hue color palette.
+palette = brewer['Purples'][8]
+
+# Reverse color order so that dark blue is highest obesity.
+palette = palette[::-1]
+
+# Add hover tool
+hover = HoverTool(tooltips = [ ('Borough','@borough'),
+                               ('Tip percentage', '@tip_p{0%}'),
+                               ('Average Tip amount', '$@tip_amount{,}'),
+                               ('Average fare amount', '$@fare_amount{,}'),
+                               ('Amount of pickups', '@pickup{,}'),
+                               ('Amount of dropoffs', '@dropoff{,}')
+                               ]
+)
+
+# Call the plotting function
+p = make_plot(input_field)
+
+# Make a slider object: slider 
+slider = Slider(title = 'Month',start = 1, end = 12, step = 1, value = 1)
+slider.on_change('value', update_plot)
+
+# Make a selection object: select
+select = Select(title='Select Criteria:', value='Tip percentage', options=['Tip percentage', 'Average Tip amount',
+                                                                               'Average fare amount', 'Amount of pickups', 'Amount of dropoffs'
+                                                                               ])
+select.on_change('value', update_plot)
+
+# Make a column layout of widgetbox(slider) and plot, and add it to the current document
+# Display the current document
+layout = column(p, widgetbox(select), widgetbox(slider))
+curdoc().add_root(layout)
+
+# Use the following code to test in a notebook
+# Interactive features will no show in notebook
+
+#output_notebook()
+#show(p)
+
+output_file('plot.html', mode='inline')
+save(p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ data_preprocessing.py
@@ -0,0 +1,256 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import shapefile
+import urllib.request
+import zipfile
+import geopandas as gpd
+import os
+import json
+from functools import reduce
+
+
+from shapely.geometry import Polygon
+
+from bokeh.io import output_notebook, show, output_file, curdoc
+from bokeh.plotting import figure
+from bokeh.models import GeoJSONDataSource, LinearColorMapper, ColorBar, NumeralTickFormatter, Slider, HoverTool, Select, Column
+from bokeh.palettes import brewer
+from bokeh.layouts import widgetbox, row, column
+from bokeh.application import Application
+from bokeh.application.handlers import FunctionHandler
+
+
+#-------------------------------------------------------------------------------
+# Helper functions
+
+def get_lat_lon(sf):
+    content = []
+    for sr in sf.shapeRecords():
+        shape = sr.shape
+        rec = sr.record
+        loc_id = rec[shp_dic['LocationID']]
+        
+        x = (shape.bbox[0]+shape.bbox[2])/2
+        y = (shape.bbox[1]+shape.bbox[3])/2
+        
+        content.append((loc_id, x, y))
+    return pd.DataFrame(content, columns=["LocationID", "longitude", "latitude"])
+
+
+#--------------------------------------------------------------------------------
+# Data loading and preprocessing
+
+date_parser = pd.to_datetime
+datatype_dict = {'VendorID': float,
+                 'tpep_pickup_datetime': str,
+                 'tpep_dropoff_datetime': str,
+                 'passenger_count': float,
+                 'trip_distance': float,
+                 'RatecodeID': float,
+                 'store_and_fwd_flag': str,
+                 'PULocationID': float,
+                 'DOLocationID': float,
+                 'payment_type': float,
+                 'fare_amount':float,
+                 'extra': float,
+                 'mta_tax': float,
+                 'tip_amount': float,
+                 'tolls_amount': float,
+                 'improvement_surcharge': float,
+                 'total_amount': float,
+                 'congestion_surcharge': float}
+
+parse_dates = ['tpep_pickup_datetime', 'tpep_dropoff_datetime']
+data_path = 'C://Users/nicol/Google Drev/DTU/MMC/Social dataanlyse og visualisering/socialdata2021/project/data/'
+
+#data_files = os.listdir(data_path)
+
+data1 = 'yellow_tripdata_2020-01.csv'
+data2 = 'yellow_tripdata_2020-02.csv'
+data3 = 'yellow_tripdata_2020-03.csv'
+
+data_files = [data1,data2,data3]
+
+df = pd.read_csv(data_path + data_files[0], dtype = datatype_dict, parse_dates = parse_dates, nrows = 1000000) #
+
+
+for file in data_files[1:]:
+    df = pd.concat([df, pd.read_csv(data_path + file, dtype = datatype_dict, parse_dates = parse_dates, nrows = 1000000)]) #
+
+
+#--------------------------------------------------------------------------------
+# Download location data
+
+if os.path.isfile('shape/taxi_zones.shp') == False:
+
+  urllib.request.urlretrieve("https://s3.amazonaws.com/nyc-tlc/misc/taxi_zones.zip", "taxi_zones.zip")
+
+  with zipfile.ZipFile("taxi_zones.zip","r") as zip_ref:
+      zip_ref.extractall("./shape")
+
+sf = shapefile.Reader("shape/taxi_zones.shp")
+fields_name = [field[0] for field in sf.fields[1:]]
+
+shp_dic = dict(zip(fields_name, list(range(len(fields_name)))))
+attributes = sf.records()
+shp_attr = [dict(zip(fields_name, attr)) for attr in attributes]
+
+df_loc = gpd.GeoDataFrame(shp_attr).join(get_lat_lon(sf).set_index("LocationID"), on="LocationID")
+df_loc.head()
+
+shh = []
+for sr in sf.shapeRecords():
+  shape = sr.shape
+  rec = sr.record
+  loc_id = rec[shp_dic['LocationID']]
+  zone = rec[shp_dic['zone']]
+  shh.append((Polygon(shape.points), loc_id))
+
+shh_d = gpd.GeoDataFrame(shh, columns = ['shapefile','LocationID']).set_index('LocationID')
+df_loc = df_loc.set_index('LocationID')
+df_loc= df_loc[~df_loc.index.duplicated(keep='first')]
+df_loc = df_loc.reset_index(drop = False)
+
+
+df_loc = (df_loc.set_index('LocationID')).merge(shh_d, how ='inner', on = 'LocationID')
+df_loc = df_loc.reset_index(drop = False)
+
+#---------------------------------------------------------------------------------
+# Map taxi zone codes to location data + compute temporal data
+
+borough_mapping = dict(zip(df_loc.LocationID, df_loc.borough))
+zone_mapping = dict(zip(df_loc.LocationID, df_loc.zone))
+
+df['PUborough'] = df['PULocationID'].map(borough_mapping)
+df['DOborough'] = df['DOLocationID'].map(borough_mapping)
+
+df['PUzone'] = df['PULocationID'].map(zone_mapping)
+df['DOzone'] = df['DOLocationID'].map(zone_mapping)
+
+df['pickup_month'] = df.tpep_pickup_datetime.dt.month
+df['dropoff_month'] = df.tpep_dropoff_datetime.dt.month
+
+#---------------------------------------------------------------------------------
+# Creating geopandas dataframe for map plotting borough
+"""
+ls = {}
+popu_pu = {}
+popu_do = {}
+
+b = ['Manhattan', 'Queens', 'Brooklyn', 'Bronx', 'Staten Island']
+
+for it in b:
+  dates = df.pickup_month[(df.tip_amount > 0 )& (df.fare_amount > 0) & (df.trip_distance < 30) & (df.PUborough == it)]
+  dates_popu_pickup = df.pickup_month[df.PUborough == it]
+  dates_popu_dropoff = df.dropoff_month[df.DOborough == it]
+
+  tip = df.tip_amount[(df.tip_amount > 0 )& (df.fare_amount > 0) & (df.trip_distance < 30) & (df.PUborough == it)]
+  fare = df.fare_amount[(df.tip_amount > 0 ) &(df.fare_amount > 0 ) & (df.trip_distance < 30)& (df.PUborough == it)]
+
+  pickup = df.PUborough[df.PUborough == it]
+  dropoff = df.DOborough[df.DOborough == it]
+
+  t = pd.concat([dates, tip, fare], axis = 1).groupby(['pickup_month']).mean()
+  t1 = pd.concat([dates_popu_pickup, pickup],axis = 1).groupby(['pickup_month']).size()
+  t2 = pd.concat([dates_popu_dropoff, dropoff],axis = 1).groupby(['dropoff_month']).size()
+
+
+  
+  t.columns = ['tip_amount', 'fare_amount']
+  t1.columns = ['pickupp']
+  t2.columns = ['dropoff']
+
+  ls[it] = t 
+  popu_pu[it] = t1
+  popu_do[it] = t2
+
+#https://towardsdatascience.com/creating-an-interactive-map-in-python-using-bokeh-and-pandas-f84414536a06
+geo = gpd.read_file('shape/taxi_zones.shp')
+
+tmp = pd.concat(ls, axis=0).reset_index(drop = False)
+tmp.columns = ['Borough', 'pickup_month', 'tip_amount','fare_amount']
+tmp1 = pd.concat(popu_pu, axis = 0).reset_index(drop= False)
+tmp1.columns = ['Borough', 'pickup_month', 'pickupp']
+tmp2 = pd.concat(popu_do, axis = 0).reset_index(drop = False)
+tmp2.columns = ['Borough', 'pickup_month', 'dropoff']
+
+
+
+data_frames = [tmp,tmp1,tmp2]
+
+df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['pickup_month','Borough'],
+                                            how='inner'), data_frames)
+
+#tmp = pd.merge([tmp,tmp1], how = 'inner')
+df_merged['tip_p'] = df_merged.tip_amount/df_merged.fare_amount
+df_merged.columns = ['borough', 'month','tip_amount','fare_amount','pickup','dropoff','tip_p']
+
+geo = gpd.read_file('shape/taxi_zones.shp')
+geo = pd.merge(geo, df_merged, how = 'inner', on = 'borough')
+
+#geo.to_csv('preprocessed_data/geo.csv', index = False)
+geo.to_file("preprocessed_data/geo.geojson", driver='GeoJSON')
+"""
+#---------------------------------------------------------------------------------
+# Creating geopandas dataframe for map plotting zone
+
+ls = {}
+popu_pu = {}
+popu_do = {}
+
+b = df['PUzone'].unique()
+
+for it in b:
+  dates = df.pickup_month[(df.tip_amount > 0 )& (df.fare_amount > 0) & (df.trip_distance < 30) & (df.PUzone == it)]
+  dates_popu_pickup = df.pickup_month[df.PUzone == it]
+  dates_popu_dropoff = df.dropoff_month[df.DOzone == it]
+
+  tip = df.tip_amount[(df.tip_amount > 0 )& (df.fare_amount > 0) & (df.trip_distance < 30) & (df.PUzone == it)]
+  fare = df.fare_amount[(df.tip_amount > 0 ) &(df.fare_amount > 0 ) & (df.trip_distance < 30)& (df.PUzone == it)]
+
+  pickup = df.PUzone[df.PUzone == it]
+  dropoff = df.DOzone[df.DOzone == it]
+
+  t = pd.concat([dates, tip, fare], axis = 1).groupby(['pickup_month']).mean()
+  t1 = pd.concat([dates_popu_pickup, pickup],axis = 1).groupby(['pickup_month']).size()
+  t2 = pd.concat([dates_popu_dropoff, dropoff],axis = 1).groupby(['dropoff_month']).size()
+
+
+  
+  t.columns = ['tip_amount', 'fare_amount']
+  t1.columns = ['pickupp']
+  t2.columns = ['dropoff']
+
+  ls[it] = t 
+  popu_pu[it] = t1
+  popu_do[it] = t2
+
+#https://towardsdatascience.com/creating-an-interactive-map-in-python-using-bokeh-and-pandas-f84414536a06
+geo = gpd.read_file('shape/taxi_zones.shp')
+
+tmp = pd.concat(ls, axis=0).reset_index(drop = False)
+tmp.columns = ['Zone', 'pickup_month', 'tip_amount','fare_amount']
+tmp1 = pd.concat(popu_pu, axis = 0).reset_index(drop= False)
+tmp1.columns = ['Zone', 'pickup_month', 'pickupp']
+tmp2 = pd.concat(popu_do, axis = 0).reset_index(drop = False)
+tmp2.columns = ['Zone', 'pickup_month', 'dropoff']
+
+
+
+data_frames = [tmp,tmp1,tmp2]
+
+df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['pickup_month','Zone'],
+                                            how='inner'), data_frames)
+
+#tmp = pd.merge([tmp,tmp1], how = 'inner')
+df_merged['tip_p'] = df_merged.tip_amount/df_merged.fare_amount
+df_merged.columns = ['zone', 'month','tip_amount','fare_amount','pickup','dropoff','tip_p']
+
+geo = gpd.read_file('shape/taxi_zones.shp')
+geo = pd.merge(geo, df_merged, how = 'inner', on = 'zone')
+print(geo)
+
+#geo.to_csv('preprocessed_data/geo.csv', index = False)
+geo.to_file("preprocessed_data/geo_zone.geojson", driver='GeoJSON')
