5cadeb94736cf8d8ed299fb151ac6b51956c26da
MikkelMathiasen23 <51484172+MikkelMathiasen23@users.noreply.github.com>
2021-05-13 16:06:40 +0200

    Update reveal.js

--- reveal.js/js/reveal.js
+++ reveal.js/js/reveal.js
@@ -215,7 +215,7 @@
 			pdfPageHeightOffset: -1,
 
 			// Number of slides away from the current that are visible
-			viewDistance: 3,
+			viewDistance: 4,
 
 			// The display mode that will be used to show slides
 			display: 'block',
